eb0b1746992cfcac300fa63b9b2c22278e4379f0
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2022-11-19 13:51:24 +0530

    Removed tape

--- test/index.js
+++
@@ -1,9 +0,0 @@
-var it = require('tape'),
-    fs = require('fs'),
-    ffmpeg = require('..');
-
-it('should find ffmpeg', function(t) {
-  var stats = fs.statSync(ffmpeg.path);
-  t.ok(stats.isFile(ffmpeg.path));
-  t.end();
-});
